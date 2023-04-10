6095fb097d47a08ce4198887b0b0fddb98169b57
Paul Okeke <pauldiconline@gmail.com>
2023-04-10 22:27:00 +0100

    Updated .env.sample

--- rentmyvr_back/settings.py
+++ rentmyvr_back/settings.py
@@ -235,10 +235,13 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+if DEBUG:
+    MIDDLEWARE.append('debug_toolbar.middleware.DebugToolbarMiddleware')
+    
 
 ROOT_URLCONF = f'{PROJECT_NAME}.urls'
 
